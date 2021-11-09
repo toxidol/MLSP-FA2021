014c1d98728289a321156b6355e12db77967287c
Gkao03 <gorek@andrew.cmu.edu>
2021-11-09 03:07:49 -0500

    add file for getting file name

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa
 import soundfile as sf
+import os
 
 
 def get_magnitude_spectrum(path, sr=None):
@@ -16,3 +17,9 @@ def get_magnitude_spectrum(path, sr=None):
 def calculate_kldiv(A, B):
     divergence = np.sum(np.multiply(A, np.log10(np.divide(A, B))) - A + B)
     return divergence
+
+
+def get_file_name(file_path):
+    basename = os.path.basename(file_path)
+    filename = basename.split('.')[0]
+    return filename + ".wav"
